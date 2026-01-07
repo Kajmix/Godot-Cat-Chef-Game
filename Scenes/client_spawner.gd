ada1a521ca9f3907dfbaff1df2d26b86b5a22bd3
Kajmix <kacpermizgala1@gmail.com>
2026-01-07 20:18:31 +0100

    Tables system and ordering system part 2

---
+++ Scenes/client_spawner.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var tables: Array = []
+var taken_tables : Array = []
+var free_tables : Array = []
+
+func _ready():
+	# Get all nodes in the "table" group
+	tables = get_tree().get_nodes_in_group("table")
+	free_tables = get_free_tables()
+	taken_tables = get_taken_tables()
+
+func get_free_tables():
+	return tables.filter(func(t): return t.is_taken == false)
+
+func get_taken_tables():
+	return tables.filter(func(t): return t.is_taken == true)
+
+func _on_client_cooldown_timeout() -> void:
+	free_tables = get_free_tables()
+	if free_tables.size() > 0:
+		var table = free_tables[randi_range(0,free_tables.size() - 1)]
+		table.show_customer()
+	else:
+		print("No free table left")
--- Scenes/table_logic.gd
+++ Scenes/table_logic.gd
@@ -4,28 +4,20 @@ extends StaticBody2D
 @onready var Customer_Sprite : AnimatedSprite2D = $Customer/AnimatedSprite2D
 @onready var Order_Item : AnimatedSprite2D = $"Customer/Order/Order Sprite"
 @onready var Order : Node2D = $Customer/Order
-var a = true
+@export var is_taken = false
 func hide_customer():
+	is_taken = false
 	Customer.hide()
 	Customer_Colision.disabled = true
+	
 func show_customer():
+	is_taken = true
 	Customer.show()
 	Customer_Colision.disabled = false
 	Customer_Sprite.frame = randi_range(0,1)
-	var wanted_food = randi_range(0,3)
-	Order_Item.frame = wanted_food
-	if Order_Item.frame == 1 || Order_Item.frame == 2 || Order_Item.frame == 3:
-		Order.hide()
-	else:
-		Order.show()
+	var wanted_food = randi_range(0,2)
+	Order_Item.frame = wanted_food[]
+	Order.show()
 	
 func _ready() -> void:
 	hide_customer()
-func _process(_delta: float) -> void:
-	if Input.is_action_pressed("interact"):
-		if a:
-			show_customer()
-			a = !a
-		else:
-			hide_customer()
-			a = !a
