5b464901d83a82673518b3f2d3f3652b72ddac58
Kajmix <kacpermizgala1@gmail.com>
2026-01-07 12:19:23 +0100

    New Tailmap, Back Sprite For Player, Ease In Out Movement

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
-
-const SPEED = 100.0
+@onready var Player_sprite : Sprite2D = $Sprite2D
+const SPEED = 50.0
 
 func _physics_process(_delta: float) -> void:
 	var dir = Input.get_vector("left", "right", "up", "down")
-	velocity = dir.normalized() * SPEED
+	if Input.is_action_pressed("up"):
+		Player_sprite.texture = load("res://Assets/Player Sprite-back.png")
+	if Input.is_action_pressed("down"):
+		Player_sprite.texture = load("res://Assets/Player Sprite.png")
+	if Input.is_action_pressed("left"):
+		Player_sprite.texture = load("res://Assets/Player Sprite.png")
+	if Input.is_action_pressed("right"):
+		Player_sprite.texture = load("res://Assets/Player Sprite.png")
+	var target = dir.normalized() * SPEED
+	velocity = velocity.lerp(target, 0.3)
 	move_and_slide()
