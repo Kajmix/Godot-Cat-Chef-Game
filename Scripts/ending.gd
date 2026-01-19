54563fb534558570cc427d6b3e98cac9162bca81
Kajmix <kacpermizgala1@gmail.com>
2026-01-19 22:18:29 +0100

    monologue - ending

--- Scripts/ending.gd
+++ Scripts/ending.gd
@@ -5,7 +5,10 @@ func _on_creator_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		Monologue.start_monologue(
 			[
-			"Oh, hi User ^^"
+			"Oh, hi!", 
+			"Did you know? You have served " + str(MainGameManager.served_customers) + " customers. Congrats!", 
+			"I want to thank you, Player, for giving my game a chance \nand for the time you have spent.", 
+			"So... thank you."
 			],
 			"The Creator"
 		)
